e733196c01dfebbe6a6379f08753c448cf4f4ee9
Templin Konstantin <1qnbhd@gmail.com>
2022-09-19 19:51:32 +0300

    Initial commit :star:

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from bifolio.main import app as application
+
+
+@pytest.fixture
+def app():
+    yield application
---
+++ tests/test_server.py
@@ -0,0 +1,8 @@
+def test_fixture_test_client_get(app):
+    """
+    Simple index test.
+    """
+
+    req, resp = app.test_client.get('/')
+    assert resp.status == 200
+    assert resp.json == {"hello": "world"}
