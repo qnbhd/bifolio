9a127b4c12247d5d088666ab6c83c602402ca609
Templin Konstantin <1qnbhd@gmail.com>
2022-09-21 18:42:12 +0300

    HW1: review fixes

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,6 +3,5 @@ def test_fixture_test_client_get(app):
     Simple index test.
     """
 
-    req, resp = app.test_client.get("/")
-    assert resp.status == 200
-    assert resp.json == {"hello": "world"}
+    req, resp = app.test_client.get("/hello")
+    assert resp.text == "Hello, world!"
